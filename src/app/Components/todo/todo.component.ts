e79adb3a044f68c20945e6e7dd73d80302e79c09
Yuvraj <kingyuvi88@gmail.com>
2023-02-28 03:45:38 +0530

    Updated Todo.

---
+++ src/app/Components/todo/todo.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import {Todo} from "../../Todo"; 
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent {
+  reminder:boolean;
+  @Input() todo:Todo; 
+}
