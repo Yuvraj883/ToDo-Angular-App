4341162f49f53b784bd07a3c5d5de68ff29679ba
Yuvraj <kingyuvi88@gmail.com>
2023-02-27 23:32:19 +0530

    Updated App component.

--- src/app/Components/todos/todos.component.ts
+++ src/app/Components/todos/todos.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import{ todo } from '../../Todo'; 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit {
+todos: todo[]; 
+constructor(){
+  
+  this.todos=[
+    {
+      sno: 1, 
+      title:"First Todo",
+      desc: "this is the first todo",
+      reminder: true
+    }
+  ]
+ 
+}
+ngOnInit() : void{
 
+}
 }
